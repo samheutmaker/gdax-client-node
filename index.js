105043c09362580a7306d50edf07d2568cdbdda2
SamHeutmaker <samheutmaker@gmail.com>
2017-06-30 00:02:52 -0700

    clean up and some logging

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// let sum = (array) => array.length ? array.pop() + sum(array) : 0;
-// let average = (array) => sum(array) / array.length;
-
-// const GdaxWebsocket = require('./lib/gdax-websocket');
-
-// var socket = new GdaxWebsocket();
-
-
-// socket.on('message', (data) => console.log(JSON.stringify(data)));
+let sum = (array) => array.length ? array.pop() + sum(array) : 0;
+let average = (array) => sum(array) / array.length;
 
 const RealtimeBook = require('./lib/realtime-book');
 
 let liveBook = new RealtimeBook();
+
+liveBook.on('message', (data) => {
+	
+});
 
  
