1af57c236b8c9cc46d3f94c581955271b96843f3
SamHeutmaker <samheutmaker@gmail.com>
2017-06-29 21:48:10 -0700

    Streaming book complete without tests or docs

--- index.js
+++ index.js
@@ -8,65 +8,8 @@
 
 // socket.on('message', (data) => console.log(JSON.stringify(data)));
 
-const Client = require('./lib/public-client');
+const RealtimeBook = require('./lib/realtime-book');
 
-let publicClient = new Client();
+let liveBook = new RealtimeBook();
 
-// publicClient.getProducts()
-// .then((products) => {
-// 	console.log(products);
-// });
-
-
-// publicClient.getBook()
-// .then((res) => {
-// 	console.log(res.body);
-// })
-// .catch((err) => console.error(err));
-
-
-// publicClient.getTrades()
-// .then((res) => {
-// 	console.log(res.body);
-// })
-// .catch((err) => console.error(err));
-
-
-// publicClient.getHistoricRates({
-// 	start: new Date('27 June 2017 14:48 UTC').toISOString(),
-// 	end: new Date('27 June 2017 16:48 UTC').toISOString(),
-// 	granularity: 60
-// })
-// .then((res) => {
-// 	console.log(res.body[0][0])
-// 	console.log(res.body[res.body.length - 1][0])
-// 	console.log(new Date(res.body[0][0]*1000));
-// 	console.log(new Date(res.body[res.body.length - 1][0]*1000));
-// })
-// .catch((err) => console.error(err));
-
-
-
-// publicClient.get24HourStats()
-// .then((res) => {
-// 	console.log(res.body);
-// })
-// .catch((err) => console.error(err));
-
-
-// publicClient.getCurrencies()
-// .then((res) => {
-// 	console.log(res.body);
-// })
-// .catch((err) => console.error(err));
-
-
-// publicClient.getTicker().then((res) => console.log(res.body));
-
-
-
-publicClient.getTime()
-.then((res) => {
-	console.log(res.body);
-})
-.catch((err) => console.error(err));
+ 
