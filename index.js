7601c152e1143d01113937b253db5f3efe253942
SamHeutmaker <samheutmaker@gmail.com>
2017-07-08 20:58:17 -0700

    Add extended getHistoric rates to allow for serialized requests for historical data

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 let sum = (array) => array.length ? array.pop() + sum(array) : 0;
 let average = (array) => sum(array) / array.length;
 
-const RealtimeBook = require('./lib/realtime-book');
+const PublicClient = require('./lib/public-client');
 
-let liveBook = new RealtimeBook();
+const publicClient = new PublicClient();
 
-liveBook.on('message', (data) => {
-	
-});
+let params = {
+  start: '2017-01-07T23:47:25.201Z',
+  end: '2017-01-09T23:47:25.201Z',
+  granularity: 60
+};
 
- 
+publicClient.getHistoricRates(params)
+  .then(res => {
+  	res.reduce((cur, next) => {
+  		let first = cur[0] - 60;
+  		let second = next[0];
+  		if( first != second) {
+  			console.log(first);
+  			console.log(second);
+  		}
+  		return next;
+  	});
+    
+  })
+  .catch(err => console.error(err));
