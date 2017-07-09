61f84dc21513cccf5334cf94b115aae9f627afa3
SamHeutmaker <samheutmaker@gmail.com>
2017-07-08 23:11:30 -0700

    Sign authenticated requests

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-let sum = (array) => array.length ? array.pop() + sum(array) : 0;
-let average = (array) => sum(array) / array.length;
+// let sum = (array) => array.length ? array.pop() + sum(array) : 0;
+// let average = (array) => sum(array) / array.length;
 
 const PublicClient = require('./lib/public-client');
 
 const publicClient = new PublicClient();
 
-let params = {
-  start: '2017-01-07T23:47:25.201Z',
-  end: '2017-01-09T23:47:25.201Z',
-  granularity: 60
-};
+// publicClient.getTime()
+// .then(r => console.log(r.body))
 
-publicClient.getHistoricRates(params)
-  .then(res => {
-  	res.reduce((cur, next) => {
-  		let first = cur[0] - 60;
-  		let second = next[0];
-  		if( first != second) {
-  			console.log(first);
-  			console.log(second);
-  		}
-  		return next;
-  	});
-    
-  })
-  .catch(err => console.error(err));
+// let params = {
+//   start: '2017-01-07T23:47:25.201Z',
+//   end: '2017-01-09T23:47:25.201Z',
+//   granularity: 60*60
+// };
+
+// publicClient.getHistoricRates(params)
+//   .then(res => {
+//   	res.reduce((cur, next) => {
+//   		let first = cur[0] - 60*60;
+//   		let second = next[0];
+//   		if( first != second) {
+//   			console.log(first);
+//   			console.log(second);
+//   		}
+//   		return next;
+//   	});
+
+//   })
+//   .catch(err => console.error(err));
+
+
+const PrivateClient = require('./lib/private-client');
+let privateClient = new PrivateClient();
+
+privateClient.getAccounts()
+.then((r) => console.log(r.body))
+.catch((r) => console.log(r.response.text))
